ef0eefc923bae6232d0d099a8abd4af4d83b5b3e
nafarin <nafarien84@gmail.com>
2018-03-12 07:23:18 -0700

    Add files via upload

---
+++ tes.js
@@ -0,0 +1,49 @@
+<div style="border-radius: 10px; border: 1px solid #e4e4e4; padding: 10px;">
+<div class="row margin-bottom-20">
+<div class="col-md-4 text-center">
+<h3>
+Advertise</h3>
+Kode Iklan Disini
+</div>
+<div class="col-md-4 margin-top-25">
+<div class="panel panel-success text-center">
+<div class="panel-heading">
+<h2>
+<i class="fa fa-shield"></i> SafeLink Converter <i class="fa fa-shield"></i></h2>
+</div>
+<div class="panel-body text-center">
+<div class="text-primary margin-bottom-20">
+Encrypting your link and protect the link from viruses, malware, thief, etc!<br />
+Made your link safe to visit.
+</div>
+<div class="progress" id="daplong">
+<div class="progress-bar progress-bar-striped active six-sec-ease-in-out" data-transitiongoal="100" role="progressbar">
+</div>
+</div>
+<button class="alert alert-dismissible alert-success btn-lg" id="download2" style="display: none;"><i aria-hidden="true" class="fa fa-thumbs-o-up"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i aria-hidden="true" class="fa fa-hand-o-down slideInDownThumb"></i></button>
+<br />
+<div class="panel-body">
+<script src="https://cdn.rawgit.com/mulyanacymot/safelinkliknew/2d2fb103/safelinknew.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://newgenesisseo.blogspot.com/p/blog-page_38.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+  </div>
+</div>
+</div>
+</div>
+<div class="col-md-4 text-center">
+<h3>
+Advertise</h3>
+Kode Iklan Disini
+</div>
+<div class="clear">
+</div>
+</div>
+</div>
+<div class="container">
+<div class="text-left">
+</div>
+</div>
